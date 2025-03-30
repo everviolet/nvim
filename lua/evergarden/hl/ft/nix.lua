ca0b29af5dcc3bf132381c2e50e91d1d3958e90f
robin <comfysage@isabelroses.com>
2025-03-30 15:08:43 +0100

    fix(ft/nix): use base evergarden syntax

--- lua/evergarden/hl/ft/nix.lua
+++ lua/evergarden/hl/ft/nix.lua
@@ -3,12 +3,6 @@
 ---@return evergarden.types.hlgroups
 return function(theme, config)
   return {
-    ['@variable.member.nix'] = { theme.blue },
-    ['@lsp.type.property.nix'] = { link = '@variable.member.nix' },
-
-    ['@variable.parameter.nix'] = { theme.red },
-    ['@lsp.type.parameter.nix'] = { link = '@variable.parameter.nix' },
-
-    ['@lsp.type.variable.nix'] = { theme.subtext0 },
+    ['@variable.parameter.builtin.nix'] = { theme.overlay0 }, -- `{ ... }:`
   }
 end
