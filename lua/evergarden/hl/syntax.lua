dedf0382fe95b9f3152b73ae309ed2b390d5c03d
robin <comfysage@isabelroses.com>
2025-03-30 15:08:43 +0100

    feat(hl/syntax): use aqua for special strings and symbols

--- lua/evergarden/hl/syntax.lua
+++ lua/evergarden/hl/syntax.lua
@@ -80,10 +80,10 @@ return function(theme, config)
     -- strings
     ['@string'] = { theme.syntax.string },
     ['@string.documentation'] = { theme.skye }, -- docstrings
-    ['@string.special'] = { link = '@punctuation.special' },
+    ['@string.special'] = { theme.aqua },
     ['@string.special.path'] = { link = '@string.special' }, -- filenames
     ['@string.special.url'] = { link = '@uri' }, -- URIs (e.g. hyperlinks)
-    ['@string.special.symbol'] = { theme.subtext0 },
+    ['@string.special.symbol'] = { link = '@string.special' }, -- symbols/atoms
     ['@string.escape'] = { theme.yellow },
     ['@string.regexp'] = { link = '@string.escape' },
     ['@character'] = { link = '@string' }, -- char literal
